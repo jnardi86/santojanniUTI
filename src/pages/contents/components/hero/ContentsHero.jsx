ce29e2185f763f418c774dc84521cbca739facc1
Informatica Departamento <informatica@santojanni.gcba.gov.ar>
2024-03-18 15:41:49 -0300

    Add new key to dataContents.json

--- src/pages/contents/components/hero/ContentsHero.jsx
+++ src/pages/contents/components/hero/ContentsHero.jsx
@@ -1,30 +1,30 @@
 import { useModulos } from "../../../../hooks/useModulos"
-import imageSepsisMobile from "../../../../assets/Images_figma/header_sepsis_mobile_max.png"
-
-
 
 const ContentsHero = () => {
 
   const { title, imageMobile } = useModulos()
-  // console.log("title del state is: ", title)
-  // console.log("La url de imageMobile es: ", imageMobile)
-  console.log("console imagemobile", imageMobile)
 
 
   return (
-    <div className="relative w-screen h-screen overflow-hidden">
-      <div className="absolute inset-0 bg-cover bg-center" >
-      {/* <div className="container mx-auto  inset-0 h-96 bg-cover bg-center"> */}
-        <img src={`${imageMobile}`} alt="" />
-        <p>En este div esta la uimagen en background</p>
-      </div>
-      {/* <div>
-        <img src={imageSepsisMobile} alt="" />
-      </div> */}
-      <h1>ContentsHero</h1>
-      <h2>{title}</h2>
+    <div className="heroContainer relative w-screen h-screen overflow-hidden">
+      <picture>
+        <source media="(max-width: 375px)" src={imageMobile} />
+      </picture>
+      
+
     </div>
   )
 }
 
 export default ContentsHero
+
+
+      {/* <div className="absolute inset-0 bg-cover bg-center" >
+
+        <img src={`${imageMobile}`} alt="" />
+        <p>En este div esta la uimagen en background</p>
+      </div>
+
+      <h1>ContentsHero</h1>
+      <h2>{title}</h2>
+     */}
