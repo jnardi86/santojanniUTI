b4233b45146302210ff0c117ff8a6b24a423cb4b
titojp <titojp2002@hotmail.com>
2024-04-04 01:57:37 -0300

    test public route

--- src/pages/contents/components/hero/ContentsHero.jsx
+++ src/pages/contents/components/hero/ContentsHero.jsx
@@ -4,7 +4,6 @@ const ContentsHero = () => {
 
   const { moduleData } = useModulos()
 
-
   return (
     <div className="heroContainer relative w-full overflow-hidden mb-20">
       <img src={moduleData.imageMobile} alt={moduleData.title} className="w-full h-full object-cover tablet:hidden" />
