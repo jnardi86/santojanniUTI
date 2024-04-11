f87c938b1e8d8045389b062334ad90081abc0a84
jnardi86 <julian.nardi@gmail.com>
2024-04-10 22:54:51 -0300

    Add carrousel to Speakers

--- src/pages/home/view/components/oradores/OradoresCard.jsx
+++ src/pages/home/view/components/oradores/OradoresCard.jsx
@@ -3,19 +3,15 @@ import CustomButton from '../../../../../components/customComponents/CustomButto
 
 const OradoresCard = ({ data }) => {
     return (
-        <div className="
-    min-w-[260px]
-    bg-DarkGreen text-White rounded-lg rounded-tl-[100px] 
-     mx-5 my-2 px-4 py-4 
-     flex flex-col justify-center items-center">
-            <img className="w-[180px] h-[180px] rounded-full self-start border-2 border-White mb-5" src={data.image} alt="Orador" />
-            <p className="pb-10">{data.name}</p>
-            <p className="pb-1">{data.cargo}</p>
-            <p className="pb-10">{data.especialidad}</p>
+        <div className="min-w-[260px] max-w-[375px] bg-DarkGreen text-White rounded-lg rounded-tl-[100px] mx-5 my-2 px-4 py-4 flex flex-col justify-center items-center">
+            <img className="w-[180px] h-[180px] rounded-full border-2 border-White mb-5" src={data.image} alt="Orador" />
+            <h3 className="font-poppins text-center font-semiBold text-2xl text-White mb-12">{data.name}</h3>
+            <p className="font-poppins font-semiBold text-lg text-White pb-1">{data.cargo}</p>
+            <p className="font-poppins font-semiBold text-lg text-White pb-10">{data.especialidad}</p>
 
             <CustomButton
-            style="mb-5"
-            label={"Ver más"}>
+                style="mb-5"
+                label={"Ver más"}>
             </CustomButton>
 
         </div>
