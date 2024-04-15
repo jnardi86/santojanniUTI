41cb24b3c0265c427c011c20c4762712c29c396f
jnardi86 <julian.nardi@gmail.com>
2024-04-15 00:06:45 -0300

    Building dropDownMenuUser

---
+++ src/components/navbar/components/DropDownMenuUser.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const DropDownMenuUser = () => {
+  return (
+    <div>
+        <div className='flex gap-2'>
+            <img src="" alt="" />
+            <span>nombre_usuario</span>
+        </div>
+        <div>
+            <ul>
+                <li>Mi perfil</li>
+                <li>Salir</li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default DropDownMenuUser
